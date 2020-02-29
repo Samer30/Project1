f0a945f56c59f3740daa906da29d31395eafc48a
SoftUltraKing <61206565+SoftUltraKing@users.noreply.github.com>
2020-02-29 02:18:17 +0200

    new task works
    Tomorrow ill re design icon and ill make buttons move to new task but the code please

--- app/src/main/java/practice/app/project/ListFragment.kt
+++ app/src/main/java/practice/app/project/ListFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,10 +19,19 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
-    }
+        var v = inflater.inflate(R.layout.fragment_list, container, false)
 
+        var obj = DatabaseHelper(activity!!)//here should be your database but it isn't working
+        var db = obj.readableDatabase
+        var cur = db.rawQuery("select * from tasks",null)
+        cur.moveToFirst()
+        var list = ArrayList<String>()
+        while(!cur.isAfterLast){
 
-
-
-}
+            list.add(cur.getString(0) + "\n" + cur.getString(1))
+            cur.moveToNext()
+            var adp= ArrayAdapter(activity!!,android.R.layout.simple_list_item_1,list)
+            listView_tasks.adapter=adp
+        }
+        return v
+}}
--- app/src/main/java/practice/app/project/NewTaskFragment.kt
+++ app/src/main/java/practice/app/project/NewTaskFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_new_task.*
+import kotlinx.android.synthetic.main.fragment_new_task.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +20,19 @@ class NewTaskFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_task, container, false)
+        var v = inflater.inflate(R.layout.fragment_new_task, container, false)
+        v.button.setOnClickListener {
+            var obj = DatabaseHelper(activity!!)
+            var db = obj.writableDatabase
+            db.execSQL("insert into tasks values(?)",
+                arrayOf(editText_newTask.text.toString()))
+            Toast.makeText(activity!!, "Saved", Toast.LENGTH_LONG).show()
+        }
+
+
+
+
+        return v
     }
 
 
